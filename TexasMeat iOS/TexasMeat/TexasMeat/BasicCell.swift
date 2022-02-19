05662d816d31f4293f43784fc45453fe44bc72b2
fLanc3GV <46271420+Flance-s@users.noreply.github.com>
2022-02-19 10:06:17 -0600

    Recreating main story board for tab bar nav

--- TexasMeat iOS/TexasMeat/TexasMeat/BasicCell.swift
+++ TexasMeat iOS/TexasMeat/TexasMeat/BasicCell.swift
@@ -11,4 +11,5 @@ import UIKit
 struct BasicCell {
     let image : UIImage
     let textLabel : String
+    let parent : String
 }
--- TexasMeat iOS/TexasMeat/TexasMeat/HomeViewController.swift
+++ TexasMeat iOS/TexasMeat/TexasMeat/HomeViewController.swift
@@ -5,42 +5,42 @@
 //  Created by Fady Faheem on 2/17/22.
 //
 
-import UIKit
-
-class HomeViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-    
-    var basicCells : [BasicCell] = [BasicCell(image: #imageLiteral(resourceName: "TexasMeatOnly"), textLabel: "Oven Roasted"),
-                                    BasicCell(image: #imageLiteral(resourceName: "TexasMeatOnly"), textLabel: "Grilled"),
-                                    BasicCell(image: #imageLiteral(resourceName: "TexasMeatOnly"), textLabel: "Pan Fried"),
-                                    BasicCell(image: #imageLiteral(resourceName: "TexasMeatOnly"), textLabel: "Seared"),
-                                    BasicCell(image: #imageLiteral(resourceName: "TexasMeatOnly"), textLabel: "Braising")]
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.register(UINib.init(nibName: "BasicTableViewCell", bundle: nil), forCellReuseIdentifier: Constants.reusableBasicCell)
-        // Do any additional setup after loading the view.
-    }
-    
-
-
-}
-
-extension HomeViewController : UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return basicCells.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let message = basicCells[indexPath.row]
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.reusableBasicCell, for: indexPath) as! BasicTableViewCell
-        cell.basicImageView.image = message.image
-        cell.titleLabel.text = message.textLabel
-        
-        return cell
-    }
-    
-    
-}
+//import UIKit
+//
+//class HomeViewController: UIViewController {
+//
+//    @IBOutlet weak var tableView: UITableView!
+//
+//    var basicCells : [BasicCell] = [BasicCell(image: #imageLiteral(resourceName: "TexasMeatOnly"), textLabel: "Oven Roasted", parent: "none"),
+//                                    BasicCell(image: #imageLiteral(resourceName: "TexasMeatOnly"), textLabel: "Grilled", parent: "none"),
+//                                    BasicCell(image: #imageLiteral(resourceName: "TexasMeatOnly"), textLabel: "Pan Fried", parent: "none"),
+//                                    BasicCell(image: #imageLiteral(resourceName: "TexasMeatOnly"), textLabel: "Seared", parent: "none"),
+//                                    BasicCell(image: #imageLiteral(resourceName: "TexasMeatOnly"), textLabel: "Braising", parent: "none")]
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        tableView.dataSource = self
+//        tableView.register(UINib.init(nibName: "BasicTableViewCell", bundle: nil), forCellReuseIdentifier: Constants.reusableBasicCell)
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
+//
+//}
+//
+//extension HomeViewController : UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return basicCells.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let message = basicCells[indexPath.row]
+//
+//        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.reusableBasicCell, for: indexPath) as! BasicTableViewCell
+//        cell.basicImageView.image = message.image
+//        cell.titleLabel.text = message.textLabel
+//
+//        return cell
+//    }
+//
+//
+//}
