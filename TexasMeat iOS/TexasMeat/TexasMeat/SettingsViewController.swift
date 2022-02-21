92ed022da847ccce4ebe5a4af0e22aafec0e7a95
fLanc3GV <46271420+Flance-s@users.noreply.github.com>
2022-02-20 23:43:30 -0600

    Finished info screen

---
+++ TexasMeat iOS/TexasMeat/TexasMeat/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  TexasMeat
+//
+//  Created by Fady Faheem on 2/20/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
